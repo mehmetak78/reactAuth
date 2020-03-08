a6bc2c4345ed62a88b21f6e8a2bf9d04995c5732
mehmetak <mehmetak78@hotmail.com>
2020-03-09 01:01:45 +0300

    First Commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
-const requireLogin = require("../auth/requireLogin");
+
 
 // /admin
 
 // Get Current User
-router.get("/adminHome",  requireLogin, (req, res) => {
+router.get("/adminHome",  (req, res) => {
     res.send("In Admin Home Page");
 });
 
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require("express");
 //const cookieSession = require("cookie-session");
 
 const keys = require("./config/keys");
-
+const requireLogin = require("./authMiddlewares/requireLogin");
 let passport;
 
 if (keys.authPassportJWT || keys.authPassportSession) {
     passport = require("passport");
-    require("./auth/passportStrategies");
+    require("./authMiddlewares/passportStrategies");
 }
 
 const app = express();
@@ -35,7 +35,7 @@ if (keys.authPassportJWT || keys.authPassportSession) {
 if (keys.jwtSecret) {
     app.use("/authjwt", require("./routes/authJWT"));
 }
-app.use("/admin", require("./routes/admin"));
+app.use("/admin", requireLogin, require("./routes/admin"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started on Port: ${PORT}`));
